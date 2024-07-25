4adc660adb4d737fb21c40f091d5e1f09f5ad970
kame2025 <144504762+kame2025@users.noreply.github.com>
2024-07-25 11:48:22 +0900

    getCoordinatesを削除

--- getCoordinates.m
+++
@@ -1,7 +0,0 @@
-function [redCoordinates] = getCoordinates(K)
-    % 赤色の座標を取得
-    [redRows, redCols] = find(K(:,:,1) == 255 & K(:,:,2) == 0 & K(:,:,3) == 0);
-
-    % 結果を表示
-    redCoordinates = [redRows, redCols];
-end
