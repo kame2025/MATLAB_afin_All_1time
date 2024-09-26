2343fb62e7fc462d8fc26be1844064eda492d5b7
kame2025 <144504762+kame2025@users.noreply.github.com>
2024-09-26 14:48:30 +0900

    videoの追加

--- load_video.m
+++ load_video.m
@@ -1,8 +1,16 @@
-function [imds, ten, plot_statas] = load_video(imageFolder, xlens, ylens, magnification, flag)
+function [imds, ten, plot_statas] = load_photo(imageFolder, xlens, ylens, magnification, flag)
+    % フレームごとの処理に対応したファイルパターンを設定します。
+    % ファイル名はフレーム番号を考慮して動的に変更します。
 
-    % 画像処理後のファイルを読み込む
-    filePattern = fullfile(imageFolder, '*.png');
+    % 動画フレームごとに保存するために動画特有の処理に対応
+    % 元の処理と同様に座標情報をフレームに適用
+
+    % フレーム用の画像処理後のファイルを読み込む
+    filePattern = fullfile(imageFolder, '*.png');  % 動画の場合、各フレームごとの処理が必要
     imageFiles = dir(filePattern);
+    
+    % 以下の処理はそのまま適用できます。
+    % 動画フレームごとの処理フローを踏襲するように修正します。
     
     if flag
         % 指定されたファイル名を作成
